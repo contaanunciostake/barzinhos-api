5be779889b1e5982c435aba82f9ba049b5f41f48
contaanunciostake <contaanunciostake@gmail.com>
2025-06-22 23:34:02 -0300

    Update user.py

--- src/models/user.py
+++ src/models/user.py
@@ -5,10 +5,10 @@ class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), nullable=True)  # Adicionado opcionalmente
+    username = db.Column(db.String(100), nullable=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(200), nullable=False)
-    role = db.Column(db.String(80), default='user')  # 'user', 'establishment', 'admin'
+    role = db.Column(db.String(80), default='user')
 
     def __repr__(self):
         return f'<User {self.email}>'
